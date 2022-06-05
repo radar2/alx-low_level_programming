4f4e3e6c4d62175b50c7b50e789255f6149d7828
radar2 <securkydroity@gmail.com>
2022-06-05 18:07:43 +0000

    lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char ch;
+    for(char = 'a'; ch < 'Z'; ch++)
+    {
+        putchar(ch);
+    }
+	return (0);
+}
