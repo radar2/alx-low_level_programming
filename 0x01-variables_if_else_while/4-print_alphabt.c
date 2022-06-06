823285bdc7a4e8e04dcdc23de0b37b638f8aaf89
radar2 <securkydroity@gmail.com>
2022-06-05 17:28:54 -0700

    alp

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		if ((ch != 101) && (ch != 113))
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
