36e73adce5f3ba07b35796c7f1054af42d851d16
radar2 <securkydroity@gmail.com>
2022-06-05 17:58:09 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	m = 48;
+	while (m < 58)
+	{
+		putchar(m);
+		m++;
+	}
+	m = 97;
+	while (m < 103)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
