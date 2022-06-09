95cc0c6d04d2ac3c82c5c34798ab9d2e970ded73
radar2 <securkydroity@gmail.com>
2022-06-09 14:44:55 -0700

    alah

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,10 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void print_alphabet(void)
 {
 	char ch;
