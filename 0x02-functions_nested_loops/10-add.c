b79119444da402f8b8749c7e2be0446b098ec81e
radar2 <securkydroity@gmail.com>
2022-06-10 08:45:58 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - alphabet lowercase
+ *
+ *@c: the char value
+ *
+ * Return: return sum a and b
+ */
+
+int add(int c)
+{
+	return ((a + b));
+}
