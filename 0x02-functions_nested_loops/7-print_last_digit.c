ee4191622ffe831cecd80bae9144ea4298faec2e
radar2 <securkydroity@gmail.com>
2022-06-09 19:47:50 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Absolute value
+ *
+ *@n: the number value
+ *
+ * Return: last digit 
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
