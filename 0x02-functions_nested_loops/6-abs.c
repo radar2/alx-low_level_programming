e275ea64467b2152530bad536708f38f995ac44b
radar2 <securkydroity@gmail.com>
2022-06-09 19:39:58 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Absolute value
+ *
+ *@n: the number value
+ *
+ * Return: Absolute value
+ */
+
+int _abs(int n)
+{
+	return abs(n);
+}
