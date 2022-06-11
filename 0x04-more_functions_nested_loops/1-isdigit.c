107b62ac10c758d0ae584924ec1221c735d32b03
radar2 <securkydroity@gmail.com>
2022-06-11 07:11:27 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+/**
+ * _isdigit - function that checks for a digit 
+ * @c: Input param
+ * Return: 1 if c is digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+}
