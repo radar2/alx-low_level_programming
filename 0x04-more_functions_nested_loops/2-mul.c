d87fb22c98b88ca27032e7ce251a87b00914ac20
radar2 <securkydroity@gmail.com>
2022-06-11 07:33:36 -0700

    multiple

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+/**
+ * mul - function that multiple two integer
+ * @a :integer one
+ * @b: integer two
+ * Return: the a multiple b
+ */
+int mul(int a, int b)
+{
+	return ((a * b));
+}
