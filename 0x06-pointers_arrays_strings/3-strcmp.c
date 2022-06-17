97c3d82e949b2875d58ca1c4b6e0192e0e55766b
radar2 <securkydroity@gmail.com>
2022-06-17 12:55:20 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,11 +4,13 @@
 /**
  * _strcmp - a function that compare two strings
  * @s1: Input
- * @s2 Input
+ * @s2: Input
  * Return: 1 true or 0 else
  */
 int _strcmp(char *s1, char *s2)
 {
+	int i;
+
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
