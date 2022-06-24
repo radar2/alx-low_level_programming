c03bb561500420a27a192bbc02acd7c7a8cd1ad5
radar2 <securkydroity@gmail.com>
2022-06-23 18:34:29 -0700

    Strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that print string in reverse
+ * @s: Input
+ * Return: void
+ */
+void _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1);
+}
