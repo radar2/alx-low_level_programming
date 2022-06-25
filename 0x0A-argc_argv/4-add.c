bd9b82283b2a37df715b85d9bc73eefe7cfe9648
radar2 <securkydroity@gmail.com>
2022-06-25 12:01:54 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that positives numbers
+ * @argc: the size of argv
+ * @argv: array of argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, res;
+
+	res = 0;
+	if (argc < 2)
+	{
+		printf("%d\n", res);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!atoi(argv[i]))
+			{
+				printf("Error\n");
+				return (EXIT_FAILURE);
+			}
+			res = res + atoi(argv[i]);
+		}
+		printf("%d\n", res);
+	}
+	return (EXIT_SUCCESS);
+}
