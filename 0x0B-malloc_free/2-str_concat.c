219e450b43e6b01f2e846516fccb324cb85a25e5
radar2 <securkydroity@gmail.com>
2022-06-28 17:57:32 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  * str_concat - get ends of input and add together for size
@@ -9,37 +10,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int end1, end2, i = 0;
-	char *array;
+	size_t i, k, len1, len2;
+	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
-
-	for (end1 = 0; end1 <= *s1; end1++)
-	{
-	}
-
-	for (end2 = 0; end2 <= *s2; end2++)
-	{
-	}
-
-	array = malloc(sizeof(char) * (end1 + end2 + 1));
-
-	if (array == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	ptr = malloc((len1 + len2) * sizeof(char));
+	if (ptr == NULL)
 		return (NULL);
-
-	while (*s1)
-	{
-		array[i] = *s1;
-		i++;
-		s1++;
-	}
-
-	while (*s2)
-	{
-		array[i] = *s2;
-		i++;
-		s2++;
-	}
-	return (array);
+	for (i = 0; i < len1; i++)
+		*(ptr + i) = *(s1 + i);
+	for (k = 0; k < len2; k++)
+		*(ptr + (len1 + k)) = *(s2 + k);
+	return (ptr);
 }
