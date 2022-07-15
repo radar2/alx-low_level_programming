5720b9399f4bb99acffed4fdb1eeaac6f763fdf2
radar2 <securkydroity@gmail.com>
2022-07-15 09:51:33 -0700

    Fix add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * add_node - Entry Point
- * @head: input
- * @str: string
- * Return: 0
+ * _strlen - gets length of string
+ * @s: string passed;
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+/**
+ * add_node - adds node to head of linked list
+ * @head: pointer to the pointer that starts the linked list
+ * @str: string value for str member
+ * Return: the address of the new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int i, count = 0;
-	list_t *new;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	if (head == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
 
-	for (i = 0; str[i] != '\0'; i++)
-		count++;
-
-	new->len = i;
-	new->str = strdup(str);
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
+	return (*head);
 }
