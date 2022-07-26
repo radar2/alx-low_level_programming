624bab5e150e1f1eab0d79ad7d6a31f7233126f0
radar2 <securkydroity@gmail.com>
2022-07-26 16:53:14 -0700

    Fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,47 @@
 #include "main.h"
-
 /**
- * read_textfile - Entry Point
- * @filename: file name
- * @letters: size
- * Return: 0
+ * read_textfile - read files w/ open/read, write text to stdout
+ * @filename: name of file passed to be read
+ * @letters: num of characters read in
+ * Return: number of letters written to stdout
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
-	char *buf;
+	char *contents;
+	ssize_t bytes, printed_letters;
+	int file_d;
 
-	if (filename == NULL)
+	contents = malloc(letters + 1);
+
+	if (contents == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
+	if (!filename)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
+	file_d = open(filename, O_RDONLY);
+
+	if (file_d == -1)
 		return (0);
 
-	rd = read(file, buf, letters);
-	if (rd == -1)
+
+	bytes = read(file_d, contents, letters);
+	if (bytes == -1)
+	{
+		close(file_d);
+		free(contents);
 		return (0);
+	}
+	contents[bytes] = '\0';
 
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-	if (wr == -1)
+	printed_letters = write(STDOUT_FILENO, contents, bytes);
+	if (printed_letters == -1)
+	{
+		close(file_d);
+		free(contents);
 		return (0);
-
-	close(file);
-	free(buf);
-	return (wr);
+	}
+	close(file_d);
+	free(contents);
+	return (printed_letters);
 }
