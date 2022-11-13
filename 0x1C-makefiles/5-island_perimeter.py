98c7078963ed3479edaaca399f7f5a9cb46882ca
radar2 <securkydroity@gmail.com>
2022-11-13 14:49:29 -0800

    Fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-"""island_perimeter module
-"""
+'''
+island perimter problem
+'''
 
 
 def island_perimeter(grid):
-    """Returns perimeter of the grid
-    """
+    '''
+    get the perimeter from island defined by grid
+    '''
+    perimeter = 0
+    for x, row in enumerate(grid):
+        for y, ele in enumerate(row):
+            if ele == 1:
+                if x == 0 or grid[x - 1][y] == 0:
+                    perimeter += 1
+                if y == 0 or grid[x][y - 1] == 0:
+                    perimeter += 1
+                if x == len(grid) - 1 or grid[x + 1][y] == 0:
+                    perimeter += 1
+                if y == len(row) - 1 or grid[x][y + 1] == 0:
+                    perimeter += 1
+    return (perimeter)
