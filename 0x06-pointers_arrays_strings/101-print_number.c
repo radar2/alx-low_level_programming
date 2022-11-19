09d1d03bf7516c4e49ba904189d57c00c6d7d22d
radar2 <securkydroity@gmail.com>
2022-11-19 08:04:27 -0500

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <limits.h>
+/**
+ * print_number - prints number using putchar
+ * @number: number getting printed
+ * Return: void
+ */
+void print_number(int number)
+{
+	char lastDigit;
+	int reversed;
+	char c;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		lastDigit = (char)('0' - (number % 10));
+		number /= -10;
+	}
+	else
+	{
+		lastDigit = (char)((number % 10) + '0');
+		number /= 10;
+	}
+	reversed = 0;
+	while (number > 0)
+	{
+		reversed = reversed * 10 + (number % 10);
+		number /= 10;
+	}
+	while (reversed > 0)
+	{
+		c = (char)((reversed % 10) + '0');
+		_putchar(c);
+		reversed /= 10;
+	}
+	_putchar(lastDigit);
+}
