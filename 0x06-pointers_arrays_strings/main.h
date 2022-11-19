f15ce69bba59f50d28827e1211ad64134873c5d9
radar2 <securkydroity@gmail.com>
2022-11-19 07:41:50 -0500

    Fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -5,3 +10,5 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char c*);
 char *cap_string(char *n)
+
+#endif /* MAIN_H */
