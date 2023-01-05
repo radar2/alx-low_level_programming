b6201e51735c6ff006f85a02b474dbc379090181
radar2 <securkydroity@gmail.com>
2023-01-04 20:06:27 -0500

    fix header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,9 @@
-#ifndef SEARCH_ALGOS
-#define SEARCH_ALGOS
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 #include <stdio.h>
 #include <math.h>
 #include <strlib.h>
 
 int linear_search(int *array, size_t size, int value);
-#endif /* SEARCH_ALGOS */
+
+#endif /* SEARCH_ALGOS_H */
