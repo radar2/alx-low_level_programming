1537de7253e10087aafa8be7f86ccae3dbc0ddf9
radar2 <45019232+radar2@users.noreply.github.com>
2024-01-10 18:42:48 +0000

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,14 +10,14 @@ int factorial(int n)
 {
 if (n < 0)
 {
-return -1;  /* Error: factorial undefined for negative numbers */
+return -1;
 }
 else if (n == 0 || n == 1)
 {
-return 1;   /* Base case: factorial of 0 or 1 is 1 */
+return 1;
 }
 else
 {
-return n * factorial(n - 1);  /* Recursive case: n! = n * (n-1)! */
+return n * factorial(n - 1);
 }
 }
