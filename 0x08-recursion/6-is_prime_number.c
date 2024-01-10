4246f16b43033c0de96e22248e04e1147062560e
radar2 <securkydroity@gmail.com>
2024-01-10 11:31:55 -0800

    is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,11 +8,11 @@
  */
 int is_prime_number(int n)
 {
-if (n <= 1)
-{
-return (0);
-
-return (is_prime_helper(n, 2));
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, 2));
 }
 
 /**
@@ -24,19 +24,19 @@ return (is_prime_helper(n, 2));
  */
 int is_prime_helper(int n, int divisor)
 {
-if (divisor <= n / 2)
-{
-if (n % divisor == 0)
-{
-return (0);
-}
-else
-{
-return (is_prime_helper(n, divisor + 1));
-}
-}
-else
-{
-return (1);
-}
+	if (divisor <= n / 2)
+	{
+		if (n % divisor == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (is_prime_helper(n, divisor + 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
 }
