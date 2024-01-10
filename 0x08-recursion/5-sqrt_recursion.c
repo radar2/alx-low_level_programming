525a4c83f7a3a2cc7fb0ee65088ee869692e2f35
radar2 <45019232+radar2@users.noreply.github.com>
2024-01-10 19:08:14 +0000

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number for which to calculate the square root.
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root.
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else
+{
+return (sqrt_helper(n, 0));
+}
+}
+
+/**
+ * sqrt_helper - Helper function to recursively calculate the square root.
+ * @n: The number for which to calculate the square root.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root.
+ */
+int sqrt_helper(int n, int guess)
+{
+if (guess * guess == n)
+{
+return (guess);
+}
+else if (guess * guess > n)
+{
+return (-1);
+}
+else
+{
+return (sqrt_helper(n, guess + 1));
+}
+}
