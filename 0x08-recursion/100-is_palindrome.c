65af4751cb9a8a1dfce90b9522b6dce3690f816a
radar2 <securkydroity@gmail.com>
2024-01-10 11:38:39 -0800

    is_palindrome Done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,14 +9,14 @@
  */
 int is_palindrome(char *s)
 {
-int len = strlen(s);
+	int len = strlen(s);
 
-if (len == 0)
-{
-return (1);
-}
+	if (len == 0)
+	{
+		return (1);
+	}
 
-return (is_palindrome_helper(s, 0, len - 1));
+	return (is_palindrome_helper(s, 0, len - 1));
 }
 
 /**
@@ -29,18 +29,18 @@ return (is_palindrome_helper(s, 0, len - 1));
  */
 int is_palindrome_helper(char *s, int start, int end)
 {
-if (start == end)
-{
-return (1);
-}
-if (s[start] != s[end])
-{
-return 0;
-}
-if (start < end + 1)
-{
-return (is_palindrome_helper(s, start + 1, end - 1));
-}
+	if (start == end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	if (start < end + 1)
+	{
+		return (is_palindrome_helper(s, start + 1, end - 1));
+	}
 
-return (1);
+	return (1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,7 @@ int _sqrt_recursion(int n);
 int sqrt_helper(int n, int guess);
 int is_prime_number(int n);
 int is_prime_helper(int n, int divisor);
+int is_palindrome(char *s);
+int is_palindrome_helper(char *s, int start, int end)
 int _putchar(char c);
 #endif /* MAIN_H */
