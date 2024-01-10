67f7b5cac4fd565e7e9ca4c4eea24ce5b67b3432
radar2 <securkydroity@gmail.com>
2024-01-10 11:13:14 -0800

    _sqrt_recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqrt_helper(int n, int guess);
 int _putchar(char c);
 #endif /* MAIN_H */
