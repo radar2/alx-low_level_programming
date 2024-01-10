86ed99b0f622c9da12db78ee02b2e2807d92f68a
radar2 <securkydroity@gmail.com>
2024-01-10 10:02:01 -0800

    Update pust

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/*
+ *_puts_recursion - function that prints a string, followed by a new line
+ *@s: argument
+ *
+ *Return: 
+ */
 void _puts_recursion(char *s) {
     if (*s == '\0') {
         _putchar('\n');
