1c280d0f4eb41c28f5ba04cc6b801de5af4caa0b
radar2 <securkydroity@gmail.com>
2024-01-10 10:36:05 -0800

    _strlen_recursion done

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
-int _strlen_recursion(char *s) {
-if (*s == '\0') {
-  return 0;
-}
-  return 1 + _strlen_recursion(s + 1);
+/**
+ * _strlen_recursion -  function that returns the length of a string
+ * @s: Agrs
+ *
+ * Return: void
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
 }
