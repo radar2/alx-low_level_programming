396877a4ba487fe0a37ee9db9c0efa3d578aaa53
radar2 <securkydroity@gmail.com>
2024-01-10 11:10:57 -0800

    _sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,14 +9,14 @@
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-return (-1);
-}
-else
-{
-return (sqrt_helper(n, 0));
-}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_helper(n, 0));
+	}
 }
 
 /**
@@ -29,16 +29,16 @@ return (sqrt_helper(n, 0));
  */
 int sqrt_helper(int n, int guess)
 {
-if (guess * guess == n)
-{
-return (guess);
-}
-else if (guess * guess > n)
-{
-return (-1);
-}
-else
-{
-return (sqrt_helper(n, guess + 1));
-}
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_helper(n, guess + 1));
+	}
 }
