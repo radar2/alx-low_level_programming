ede344983565fd66dc894ec1f622ac680e20ca13
radar2 <securkydroity@gmail.com>
2024-01-15 02:07:46 -0800

    update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Script  creates a static library called liball
 
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-ar rcs liball.a *.o
-rm -f *.o
+gcc -Wall -Wextra -pedantic -std=gnu89 -c *.c
+ar rcs libmy.a *.o
 
