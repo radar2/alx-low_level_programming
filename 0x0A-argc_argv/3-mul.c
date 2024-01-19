44ad25ee4c2fea79378d72ee6e61b689f5617fe4
radar2 <securkydroity@gmail.com>
2024-01-19 17:22:44 +0000

    solved

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,14 +10,15 @@
  */
 int main(int argc  __attribute__((unused)), char *argv[])
 {
+	int num1, num2;
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
 	printf("%d\n", num1 * num2);
 
