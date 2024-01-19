ef5466cfc74ca97da5e4d96869b98c51681840e9
radar2 <securkydroity@gmail.com>
2024-01-19 17:08:28 +0000

    solved

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return 0;
+}
