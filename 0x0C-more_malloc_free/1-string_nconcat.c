5d1eebfffcaa72f2bdfd81a208231257d713c5f0
radar2 <securkydroity@gmail.com>
2024-01-19 18:27:47 +0000

    -solved

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,19 +15,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *concatenated;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
+
 	if (n >= len2)
+	{
 		n = len2;
+	}
 	concatenated = malloc(len1 + n + 1);
 
 	if (concatenated == NULL)
-		return NULL;
+	{
+		return (NULL);
+	}
 	strcpy(concatenated, s1);
 
 	strncat(concatenated, s2, n);
