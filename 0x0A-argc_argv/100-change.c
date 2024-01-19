89f9521f474c16bba70ad4bed900b13779bde5a5
radar2 <securkydroity@gmail.com>
2024-01-19 17:37:08 +0000

    solved

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int[] coins;
+	int num_coins, count, cents;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	coins[] = {25, 10, 5, 2, 1};
+	num_coins = sizeof(coins) / sizeof(coins[0]);
+	count = 0;
+
+	for (int i = 0; i < num_coins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
+	}
+
+	printf("%d\n", count);
+
+	return (0);
+}
