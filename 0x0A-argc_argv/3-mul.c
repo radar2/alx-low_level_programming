fd5c8ffba33767c4c88fd2a73bb375f484de044b
radar2 <securkydroity@gmail.com>
2024-01-19 17:12:09 +0000

    solved

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
