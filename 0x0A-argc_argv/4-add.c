5d20e657bbc420bf84b69027a0959d4f321071d7
radar2 <securkydroity@gmail.com>
2024-01-19 17:29:29 +0000

    solved

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+/**
+ * is_positive_number - Check if a string is a positive number
+ * @str: The string to check
+ *
+ * Return: 1 if the string is a positive number, 0 otherwise
+ */
+int is_positive_number(const char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (!is_positive_number(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
