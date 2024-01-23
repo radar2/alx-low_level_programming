fd5b10af1079fa191b3ca5ab791fb359ec2177e8
radar2 <securkydroity@gmail.com>
2024-01-23 18:15:42 +0000

    solved

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stlib.h>
-#include <stdio.h>
 
+/**
+ * dog - Represents information about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
 struct dog
 {
 	char *name;
