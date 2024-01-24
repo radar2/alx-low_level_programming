2fbd0ff6b05eb1d4c495b0d3b7effd09e9988632
radar2 <securkydroity@gmail.com>
2024-01-24 19:25:09 +0000

    done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * print_opcodes - FUnction
+ * @num_bytes: Argus
+ *
+ * Return: void
+ */
+void print_opcodes(int num_bytes) {
+    for (int i = 0; i < num_bytes; i++) {
+        printf("%02x", ((unsigned char *)print_opcodes)[i]);
+    }
+
+    printf("\n");
+}
