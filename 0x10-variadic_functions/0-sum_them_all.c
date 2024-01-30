b77be66f102dadf97aa9a18cdc3019a4c1612223
radar2 <securkydroity@gmail.com>
2024-01-30 17:57:21 +0000

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,24 +8,22 @@
  * @n: variadic inputs
  * Return: sum
  */
-int sum_them_all(const unsigned int n, ...)
-{
-	va_list valist;
+int sum_them_all(const unsigned int n, ...) {
 	unsigned int i, sum;
+	va_list args;
 
 	sum = 0;
 
-	if (n == 0)
+	va_start(args, n);
+	if (n == 0) {
 		return (0);
-
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(valist, const unsigned int);
 	}
 
-	va_end(valist);
+	for (i = 0; i < n; i++) {
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
 
 	return (sum);
 }
